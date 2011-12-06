99a6fc57ac05de07444fbd78f45eee090f98418c
calonso <mrcalonsoperez@gmail.com>
2011-12-06 12:36:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valueperdido::Application.config.secret_token = '840797cd57e96fa626df45a4775d7fbc2e50df519433619bcfaac2f5825316064920ed29ab50bde5cba57f72a1fc10af58495bd45581512a4317a145db9557c5'
