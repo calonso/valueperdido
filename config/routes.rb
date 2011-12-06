c515190626c9f2a52a1b6b17852f7e5c683c8aeb
calonso <mrcalonsoperez@gmail.com>
2011-12-06 17:17:56 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Valueperdido::Application.routes.draw do
 
-  get "pages/home"
-  get "pages/terms"
+  match '/terms', :to => 'pages#terms'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,7 +50,7 @@ Valueperdido::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
