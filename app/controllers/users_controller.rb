ecc6fdd67fc2baf65b70d06f7888ca63a31ec398
calonso <mrcalonsoperez@gmail.com>
2011-12-08 10:42:35 +0100

    users signup tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
     @title = "Sign up"
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to ValuePerdido Community!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,12 +5,12 @@ describe UsersController do
 
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector('title', :content => "Sign up")
     end
   end
@@ -28,6 +28,50 @@ describe UsersController do
     it "should find the right user" do
       get :show, :id => @user
       assigns(:user).should == @user
+    end
+  end
+
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = {:name => "", :surname => "", :email => "",
+                 :password => "", :password_confirmation => ""}
+      end
+
+      it "should not create a new user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+      before(:each) do
+        @attr = {:name => "username", :surname => "usersurname",
+                  :email => "user@example.com", :password => "UserPAssw0rd",
+                  :password_confirmation => "UserPAssw0rd"}
+      end
+
+      it "should create a new user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user's show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to ValuePerdido Community!/i
+      end
     end
   end
 
