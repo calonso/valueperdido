ca43fe0c7e151428ef42bdbc45980f113e4fce73
calonso <mrcalonsoperez@gmail.com>
2011-12-08 12:55:59 +0100

    added edit/update

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -75,4 +75,73 @@ describe UsersController do
     end
   end
 
+  describe "GET 'edit'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_login(@user)
+    end
+
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector('title', :content => 'Edit user')
+    end
+  end
+
+  describe "PUT 'update'" do
+
+    before(:each) do
+      @user = Factory(:user)
+      test_login(@user)
+    end
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :email => "", :name => "", :surname => "",
+                  :password => "", :password_confirmation => "" }
+      end
+
+      it "should render the 'edit' page" do
+        put :update, :id => @user, :user => @attr
+        response.should render_template('edit')
+      end
+
+      it "should have the right title" do
+        put :update, :id => @user, :user => @attr
+        response.should have_selector("title", :content => "Edit user")
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New Name", :email => "user@example.org", :surname => "surname",
+                  :password => "TheNewPassw0rd", :password_confirmation => "TheNewPassw0rd" }
+      end
+
+      it "should change the user's attributes" do
+        put :update, :id => @user, :user => @attr
+        @user.reload
+        @user.name.should  == @attr[:name]
+        @user.surname.should == @attr[:surname]
+        @user.email.should == @attr[:email]
+      end
+
+      it "should redirect to the user show page" do
+        put :update, :id => @user, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+
+      it "should have a flash message" do
+        put :update, :id => @user, :user => @attr
+        flash[:success].should =~ /updated/
+      end
+    end
+  end
+
 end
